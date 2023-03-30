22f80b98c1c3c57bb9882048ba6ef75ccea8ffd3
Eabishen <ebi4lbe@gmail.com>
2023-03-30 17:03:20 +0530

    Added Navbar, Reworked Service page, Reworked Process page, Added contact us, Added letstalk, Added Footer page, Added Responsiveness

--- js/main.js
+++ js/main.js
@@ -2,17 +2,85 @@ const navbar = document.querySelector('#NavBar');
 const mainCover = document.querySelector('#main-cover');
 const head = document.querySelector('#headed')
 
-console.log(head.clientHeight)
-const innerHei = head.clientHeight;
-mainCover.style.paddingTop = innerHei + "px";
+// // console.log(head.clientHeight)
+// const innerHei = head.clientHeight;
+// mainCover.style.paddingTop = innerHei + "px";
 
 function stickynavbar() {
     if (window.scrollY >= 10) {
-        navbar.classList.add('stickybox');
+        head.classList.add('stickybox');
 
     } else {
-        navbar.classList.remove('stickybox');
+        head.classList.remove('stickybox');
 
     }
 }
 window.addEventListener('scroll', stickynavbar);
+
+/*=============== SHOW MENU ===============*/
+const custNavbar = document.querySelector('.header');
+const navMenu = document.querySelector('.nav__menu');
+
+const headhieght = custNavbar.clientHeight;
+// navMenu.style.top = headhieght + "px";
+
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+    toggle.addEventListener('click', () => {
+        console.log("clicked");
+        nav.classList.toggle("show-menu")
+
+        toggle.classList.toggle('show-icon')
+    })
+}
+
+showMenu('nav-toggle', 'nav-menu')
+
+/*=============== SHOW DROPDOWN MENU ===============*/
+const dropdownItems = document.querySelectorAll('.dropdown__item')
+
+dropdownItems.forEach((item) => {
+    const dropdownButton = item.querySelector('.dropdown__button')
+
+    dropdownButton.addEventListener("click", () => {
+        const showDropdown = document.querySelector('.show-dropdown')
+        toggleItem(item)
+        if (showDropdown && showDropdown != item) {
+            toggleItem(showDropdown)
+        }
+    })
+})
+
+const toggleItem = (item) => {
+    const dropdownContainer = item.querySelector(".dropdown__container")
+
+    if (item.classList.contains('show-dropdown')) {
+        dropdownContainer.removeAttribute("style")
+        item.classList.remove("show-dropdown")
+    } else {
+
+        dropdownContainer.style.height = dropdownContainer.scrollHeight + 'px'
+        item.classList.add('show-dropdown')
+    }
+}
+
+/*=============== DELETE DROPDOWN STYLES ===============*/
+
+const mediaQuery = matchMedia('(min-width:1118px)'),
+    dropdownContainer = document.querySelectorAll('.dropdown__container')
+
+
+const removeStyle = () => {
+    if (mediaQuery.matches) {
+        dropdownContainer.forEach(e => {
+            e.removeAttribute('style')
+        })
+
+        dropdownItems.forEach(e => {
+            e.classList.remove("show-dropdown")
+        })
+    }
+}
+
+addEventListener("resize", removeStyle)
